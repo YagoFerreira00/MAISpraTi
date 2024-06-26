69acdff8b27fba824615daf82723c5e3ee041de6
YagoFerreira00 <yago.ferreira.yf00@gmail.com>
2024-06-25 23:41:59 -0300

    branch dev

---
+++ MODULO-03/ATIVIDADE-03/js/app2.js
@@ -0,0 +1,26 @@
+function saibaMais() {
+    let overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+    show.innerHTML = `<p>Obrigado por entrar em contato, ${name}!</p>
+                      <button class="btn-close" onclick="closeResult()">Fechar</button>`
+    show.style.display = "block";
+}
+function closeResult() {
+    let overlay = document.getElementById("overlay");
+    overlay.style.display = "none";
+
+    let show = document.getElementById("result");
+    show.style.display = "none";
+}
+
+function recarregarPagina() {
+    window.location.reload(); 
+}
+window.onload = function () {
+
+    window.scrollTo({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth' 
+    });
+};
